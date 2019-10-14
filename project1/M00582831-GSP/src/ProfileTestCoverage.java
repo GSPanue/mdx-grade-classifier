230715eb3f4cbe96f0e82922cd1df3937da421e5
Gurdev S. Panue <gpanue@hotmail.com>
2019-10-14 01:31:25 +0100

    Added more tests for isClear and used @ParameterizedTest instead

--- project1/M00582831-GSP/src/ProfileTestCoverage.java
+++ project1/M00582831-GSP/src/ProfileTestCoverage.java
@@ -1,38 +1,31 @@
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class ProfileTestCoverage {
-  @Test
-  // Test isClear returns the correct truth value for lower second class profile
-  public void testIsClearWithLowerSecondClassProfile() {
+  @ParameterizedTest
+  @CsvSource({
+      "8, 8, 8, 8, true", // Clear upper second class profile
+      "8, 8, 16, 16, false", // Borderline upper second class profile
+      "12, 12, 12, 12, true", // Clear lower second class profile
+      "12, 12, 16, 16, true", // Clear lower second class profile
+      "16, 16, 16, 16, true" // Clear third class profile
+  })
+  // Test isClear returns the correct truth value
+  public void testIsClear(String grade1, String grade2, String grade3, String grade4, String expectedResult) {
     List<Grade> grades = new ArrayList<>();
 
-    grades.add(new Grade(12));
-    grades.add(new Grade(12));
-    grades.add(new Grade(12));
-    grades.add(new Grade(12));
+    grades.add(new Grade(Integer.parseInt(grade1)));
+    grades.add(new Grade(Integer.parseInt(grade2)));
+    grades.add(new Grade(Integer.parseInt(grade3)));
+    grades.add(new Grade(Integer.parseInt(grade4)));
 
     Profile profile = new Profile(grades);
 
-    assertTrue(profile.isClear());
-  }
-
-  @Test
-  // Test isClear returns the correct truth value for third class profile
-  public void testIsClearWithThirdClassProfile() {
-    List<Grade> grades = new ArrayList<>();
-
-    grades.add(new Grade(16));
-    grades.add(new Grade(16));
-    grades.add(new Grade(16));
-    grades.add(new Grade(16));
-
-    Profile profile = new Profile(grades);
-
-    assertTrue(profile.isClear());
+    assertEquals(Boolean.parseBoolean(expectedResult), profile.isClear());
   }
 }
